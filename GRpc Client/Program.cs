512aafdf2196538d0f76db32eaf66fa06779cf1e
AminMohamed <xamin166@gmail.com>
2024-05-05 15:45:18 +0200

    gRPC Client

---
+++ GRpc Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+
+namespace GrpcClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter Any Key To Cont");
+            Console.ReadLine();
+            using var channel1 = GrpcChannel.ForAddress("https://localhost:7194");
+            var client = new Greeter.GreeterClient(channel1);
+            var replay =await client.SayHelloAsync(new HelloRequest { Name = "Greeter Client" });
+            Console.WriteLine($"Greetings : {replay.Message}");
+            Console.WriteLine("Enter Any Key To Exit");
+            Console.ReadLine();
+        }
+    }
+}
